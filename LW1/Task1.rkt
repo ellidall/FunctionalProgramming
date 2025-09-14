1dbd6e1165f2c398da0f90fb5e61b6fb4a829556
alexander.apakaev <alexander.apakaev@ispring.com>
2025-09-15 00:23:09 +0300

    Конечные LW1-1 и LW2-1

--- LW1/Task1.rkt
+++ LW1/Task1.rkt
@@ -1,5 +1,6 @@
  #lang racket
 
+;1
 (define (second-to-last lst)
   (cond
     [(or (empty? lst) (empty? (rest lst))) 
@@ -8,10 +9,11 @@
      (first lst)]
     [else (second-to-last (rest lst))]))
 
-;(second-to-last '(a b c d))
+(second-to-last '(a b c d))
 
 
 
+;2
 (define (my-reverse lst)
   (define (helper src acc)
     (if (empty? src)
@@ -19,20 +21,22 @@
         (helper (rest src) (cons (first src) acc))))
   (helper lst '()))
 
-;(my-reverse '(a b c d))
+(my-reverse '(a b c d))
 
 
 
+;3
 (define (my-flatten lst)
   (cond
     [(empty? lst) '()]
     [(list? (first lst)) (append (my-flatten (first lst)) (my-flatten (rest lst)))]
     [else (cons (first lst) (my-flatten (rest lst)))]))
 
-;(my-flatten '(a (b (c d) e)))
+(my-flatten '(a (b (c d) e)))
 
 
 
+;4
 (define (my-encode lst)
   (define (helper current count res)
     (cond
@@ -43,12 +47,14 @@
       [else
        (cons (list current count)(helper (first res) 1 (rest res)))]))
   (if (empty? lst)
-      (empty)
+      '()
       (helper (first lst) 1 (rest lst))))
     
-;(my-encode '(a a a a b c c a a d e e e e))
+(my-encode '(a a a a b c c a a d e e e e))
 
 
+
+;5
 (define (my-decode lst)
   (define (helper elem n)
     (if (zero? n)
@@ -59,18 +65,22 @@
       (append (helper (first (first lst)) (second (first lst)))
               (my-decode (rest lst)))))
 
-;(my-decode '((a 4) (b 1) (c 2) (a 2) (d 1) (e 4)))
+(my-decode '((a 4) (b 1) (c 2) (a 2) (d 1) (e 4)))
 
 
 
+;6
 (define (my-split lst n)
-  (define (helper current count res)
-    (if (zero? count)
-        (list current res) 
-        (helper (append current (list (first res))) (- count 1) (rest res))))
-  (helper '() n lst))
+  (cond
+    [(< n 0) (error "Индекс не может быть отрицательным")]
+    [else
+     (define (helper current count res)
+       (if (zero? count)
+           (list current res) 
+           (helper (append current (list (first res))) (- count 1) (rest res))))
+     (helper '() n lst)]))
 
-;(my-split '(a b c d e f g h i k) 3)
+(my-split '(a b c d e f g h i k) 3)
 
 
 
--- LW2/Task1.rkt
+++ LW2/Task1.rkt
@@ -1,23 +1,26 @@
 #lang racket
 
-;1 Не работает
+;1
 (define (second-to-last lst)
   (if (or (empty? lst) (empty? (rest lst)))
       (error "Список слишком короткий")
       (car (foldr (lambda (x acc)
                     (cond
-                      [(empty? acc) (list x '())]
-                      [(empty? (rest acc)) (list (car acc) x)]
-                      [else (cons (car acc) (cdr acc))]))
+                      [(empty? acc) (list x 1)]
+                      [(= (cadr acc) 2) acc]
+                      [else (list x (+ (cadr acc) 1))]))
                   '()
                   lst))))
+(second-to-last '(a b c d))
 
-;(second-to-last '(a b c d))
+
 
 ;2
 (define (my-reverse lst)
-  (foldl (lambda (x acc) (cons x acc)) '() lst))
-;(my-reverse '(a b c d))
+  (foldl cons '() lst))
+(my-reverse '(a b c d))
+
+
 
 ;3
 (define (my-flatten lst)
@@ -25,6 +28,49 @@
     [(list? lst)
      (apply append (map my-flatten lst))]
     [else (list lst)]))
-;(my-flatten '(a (b (c d) e)))
+(my-flatten '(a (b (c d) e)))
+
+
 
 ;4
+(define (my-encode lst)
+  (foldr
+   (lambda (x acc)
+     (if (null? acc)
+         (list (list x 1))
+         (let ((head (car acc)))
+           (if (equal? (car head) x)
+               (cons (list x (+ 1 (cadr head))) (cdr acc))
+               (cons (list x 1) acc)))))
+   '()
+   lst))
+(my-encode '(a a a a b c c a a d e e e e))
+
+
+
+;5
+(define (my-decode encoded)
+  (apply append
+         (map (lambda (pair)
+                (make-list (cadr pair) (car pair)))
+              encoded)))
+(my-decode '((a 4) (b 1) (c 2) (a 2) (d 1) (e 4)))
+
+
+
+;6
+(define (my-split lst n)
+  (cond
+    [(< n 0) (error "Индекс не может быть отрицательным")]
+    [else
+     (let ((result (foldl (lambda (x acc)
+                           (let ((count (car acc))
+                                 (left (cadr acc))
+                                 (right (caddr acc)))
+                             (if (< count n)
+                                 (list (+ count 1) (cons x left) right)
+                                 (list (+ count 1) left (cons x right)))))
+                         (list 0 '() '())
+                         lst)))
+       (list (reverse (cadr result)) (reverse (caddr result))))]))
+(my-split '(a b c d e f g h i k) 3)
